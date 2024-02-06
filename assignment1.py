b2384a39346b6dc6dc09589ef31ca7a02141c4a2
Nolan Whittaker <nolan_whittaker@sfu.ca>
2024-02-06 11:35:38 -0800

    truly finished ;3

--- assignment1.py
+++ assignment1.py
@@ -3,7 +3,8 @@ import numpy as np
 import wave
 from PIL import Image, ImageTk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from tkinter import *   
+from tkinter import *
+import PIL.Image   
 
 layout = [
     [sg.Text("Welcome to the .wav and .tiif reader: \n")],
@@ -19,26 +20,21 @@ def read_wav(file_path):
         print("Number of Frames: " + str(wav_file.getnframes()))
         audio_data = wav_file.readframes(wav_file.getnframes())
         audio_data = np.frombuffer(audio_data, dtype=np.int16)
-
         channel1 = audio_data[::wav_file.getnchannels()]
         channel2 = audio_data[1::wav_file.getnchannels()]
         chan1 = np.array(channel1)
         chan2 = np.array(channel2)
-
         top = Tk()
         normalized_chan1 = (chan1 - np.min(chan1)) / (np.max(chan1) - np.min(chan1))
         normalized_chan2 = (chan2 - np.min(chan2)) / (np.max(chan2) - np.min(chan2))
         min_length = min(len(normalized_chan1), len(normalized_chan2))
         C = Canvas(top, bg="black", height=1000, width=1000)
-
         for i in range(min_length - 1):
             x1 = i * 1000 / min_length
             y1 = normalized_chan1[i] * 300 + 100
             x2 = (i + 1) * 1000 / min_length
             y2 = normalized_chan1[i + 1] * 300 + 100
             line = C.create_line(x1, y1, x2, y2, fill="red")
-
-        
         for i in range(min_length - 1):
             x1 = i * 1000 / min_length
             y1 = normalized_chan2[i] * 300 + 500 
@@ -48,12 +44,10 @@ def read_wav(file_path):
         
         C.create_text(50, 50, anchor=W, text="Channel 1 is red", fill="red")
         C.create_text(50, 70, anchor=W, text="Channel 2 is blue", fill="blue")
-        
         C.pack()
         top.title("Left")
-
         layout1 = [
-            [sg.Text(".wav Plot")],
+            [sg.Text("Current File Path: " + file_path)],
             [sg.Text("Sample No.:" + str(wav_file.getnframes()))],
             [sg.Text("Sample Freq.:" + str(wav_file.getframerate()))],
             [sg.Button("Back")]
@@ -68,7 +62,7 @@ def read_wav(file_path):
         window1.close()
 
 def read_tif(file_path):
-    im = Image.open(file_path)
+    im = PIL.Image.open(file_path)
     layout2 = [
         [sg.Text(".tiff ")],
         [sg.Text("Current File Path: " + file_path)],
@@ -89,7 +83,7 @@ def read_tif(file_path):
 
         if event == "Go":
             input_file_path = values["-INIMG-"]
-            im = Image.open(input_file_path) 
+            im = PIL.Image.open(input_file_path)
             image = ImageTk.PhotoImage(image=im)
             window2['IMAGE'].update(data=image)
 
